cbd44eb8e5c8fc965c4d994e9c917e0a8346f66e
xiaoyijie <xiaoyijie2007@126.com>
2018-02-06 08:52:46 +0800

    update the test.php

--- test.php
+++ test.php
@@ -18,5 +18,34 @@
 		}
 		return base64_encode($cryptedstr);
 	}
+
+	function decrypt ($str)
+	{
+		srand(3284724);
+		if(preg_match('%^[a-zA-Z0-9/+]*={0,2}$%',$str))
+		{
+			$str = base64_decode($str);
+			if ($str != "" && $str != null && $str != false)
+			{
+				$decStr = "";
+				
+				for ($i=0; $i < strlen($str); $i+=3)
+				{
+					$array[$i/3] = substr($str,$i,3);
+				}
+
+				foreach($array as $s)
+				{
+					$a = $s ^ rand(0, 255);
+					$decStr .= chr($a);
+				}
+				
+				return $decStr;
+			}
+			return false;
+		}
+		return false;
+	}
 	echo encrypt("Admin");
+	echo decrypt("MDQyMjExMDE0MTgyMTQw")
 ?>
